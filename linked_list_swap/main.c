8f4f5eff4d7b555fcc7737b2c8ae517dbedbb351
Shengwen Cheng <shengwen1997.tw@gmail.com>
2016-07-16 01:50:12 +0800

    Finished linked list swap homework

---
+++ linked_list_swap/main.c
@@ -0,0 +1,121 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+typedef struct _List {
+	struct _List *next;
+	int val;
+} List;
+
+/* Append the linked list and return new element's address  */
+List* list_append(List **list, int val)
+{
+	//Create new element
+	List *new_element = (List *)malloc(sizeof(List));
+
+	new_element->val = val;
+	new_element->next = NULL;
+
+	List *cur = *list;
+
+	//Empty list
+	if(cur == NULL) {
+		*list = new_element;
+		return new_element;
+	}
+
+	//Go to the last item
+	while(cur->next != NULL) {
+		cur = cur->next;
+	}
+
+	//Append
+	cur->next = new_element;
+
+	return new_element;
+}
+
+/* Traversal  print elements */
+int print_list(List *list)
+{
+	assert(list);
+
+	List *cur = list;
+
+	
+	while(cur->next != NULL) {
+		printf("%d ", cur->val);
+		cur = cur->next;
+	}
+
+	printf("%d\n", cur->val);
+}
+
+/* List element swap  */
+int list_swap(List **head, List *a, List *b)
+{
+	assert(head);
+	assert(a);
+	assert(b);
+	assert(a != b);
+
+	List new_head;
+	new_head.next = *head;
+
+	/* Do traversal */
+	List *cur = &new_head;
+	
+	/* Find the previous element of a */
+	List *a_previous, *b_previous;
+
+	while(cur->next != a) {
+		cur = cur->next;
+		if(cur == NULL) {
+			return 1;
+		}
+	}
+	a_previous = cur;
+
+	/* Find the previous element of b */
+	while(cur->next != b) {
+		cur = cur->next;
+		if(cur == NULL) {
+			return 1;
+		}
+	}
+	b_previous = cur;
+		
+	/* Swap */
+	a_previous->next = b;
+	b_previous->next = a;
+	List *tmp = a->next;
+	a->next = b->next;
+	b->next = tmp;
+
+	*head = new_head.next;
+}
+
+int main(void)
+{
+	List *list = NULL;
+
+	/* Linked list creation */
+	List *a = list_append(&list, 8);
+	list_append(&list, 9);
+	list_append(&list, 1);
+	list_append(&list, 4);
+	list_append(&list, 2);
+	List *b = list_append(&list, 5);
+
+	printf("Befor swap:\n");
+	print_list(list);
+
+	/* Swap elements */
+	list_swap(&list, a, b);
+
+	printf("After swap:\n");
+	print_list(list);
+
+	return 0;
+}
+
